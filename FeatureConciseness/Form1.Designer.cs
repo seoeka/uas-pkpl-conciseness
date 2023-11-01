8927f868dbd0381e9b75359efadf4b015e6dd875
Eka <21106050023@student.uin-suka.ac.id>
2023-11-01 11:22:39 +0700

    Add project files.

---
+++ FeatureConciseness/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FeatureConciseness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_upload = new System.Windows.Forms.Button();
+            this.bt_calculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(148, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conciseness";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bt_upload
+            // 
+            this.bt_upload.Location = new System.Drawing.Point(351, 165);
+            this.bt_upload.Name = "bt_upload";
+            this.bt_upload.Size = new System.Drawing.Size(91, 30);
+            this.bt_upload.TabIndex = 1;
+            this.bt_upload.Text = "Upload File";
+            this.bt_upload.UseVisualStyleBackColor = true;
+            // 
+            // bt_calculate
+            // 
+            this.bt_calculate.Location = new System.Drawing.Point(205, 229);
+            this.bt_calculate.Name = "bt_calculate";
+            this.bt_calculate.Size = new System.Drawing.Size(140, 30);
+            this.bt_calculate.TabIndex = 2;
+            this.bt_calculate.Text = "Calculate Conciseness";
+            this.bt_calculate.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(87, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(355, 116);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Features (Functions) Total :\r\nLine of Code :\r\nConciseness : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bt_calculate);
+            this.Controls.Add(this.bt_upload);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_upload;
+        private System.Windows.Forms.Button bt_calculate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
